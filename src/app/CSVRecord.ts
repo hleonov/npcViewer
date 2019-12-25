5b57253e98f8fe52e0e399309dbac96f678ec1c3
Hadas Leonov <hadas.leonov@bruker.com>
2019-12-25 22:10:33 +0100

    initial commit

---
+++ src/app/CSVRecord.ts
@@ -0,0 +1,19 @@
+export class CSVRecord {
+
+  public number: number;
+  public firstName: string;
+  public lastName: string;
+  public gender: string;
+  public race: string;
+  public nationality: string;
+  public religion: string;
+  public class: string;
+  public looks: string;
+  public role: string;
+  public context: string;
+  public source: string;
+
+  constructor() {
+
+  }
+}
